abb08cc861e83e36e7ad6a94d93a1fa765b3ad8d
Drew Bradley <williamandrewb@gmail.com>
2021-03-03 13:45:30 -0700

    Fix Lyrics name.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Chord } from "tonal"
 import * as Key from "tonal-key";
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom'
+import Song from './Song'
 
 function App() {
   const keyChords = Key.chords("C major")
   return (
     <div className="App">
       <h1>{keyChords}</h1>
+      <Switch>
+        <Route path="/" component={ Song }/>
+        <Route path="/poem" component={ Poem }/>
+        <Route path="/saved" component={ Saved }/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Poem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Poem.css';
+
+const Poem = () => {
+  return (
+    <div className="Poem">
+      <h1>Poem</h1>
+    </div>
+  );
+}
+
+export default Poem;
