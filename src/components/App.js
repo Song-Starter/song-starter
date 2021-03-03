f7c91c5f1758d325c13fc0f1af7ebebd9c3882f7
Drew Bradley <williamandrewb@gmail.com>
2021-03-03 13:46:47 -0700

    Build 'Poem' and 'Saved' files, and build router.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import * as Key from "tonal-key";
 import './App.css';
 import {Switch, Route, Link} from 'react-router-dom'
 import Song from './Song'
+import Poem from './Poem'
+import Saved from './Saved'
 
 function App() {
   const keyChords = Key.chords("C major")
