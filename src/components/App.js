0e2ec8421ff0b4fb16582a6fa0b59759cb992d4a
Drew Bradley <williamandrewb@gmail.com>
2021-03-03 13:52:45 -0700

    Add link to Header title to navigate home.

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,15 @@ import {Switch, Route} from 'react-router-dom'
 import Song from './Song'
 import Poem from './Poem'
 import Saved from './Saved'
+import Header from './Header'
 
 function App() {
   const keyChords = Key.chords("C major")
   return (
     <div className="App">
-      <h1>{keyChords}</h1>
+      <Header />
       <Switch>
-        <Route path="/" component={ Song }/>
+        <Route exact path="/" component={ Song }/>
         <Route path="/poem" component={ Poem }/>
         <Route path="/saved" component={ Saved }/>
       </Switch>
