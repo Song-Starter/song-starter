dc1d448266ea87691f66f7e711e92ab2ac240270
Drew Bradley <williamandrewb@gmail.com>
2021-03-03 17:59:10 -0700

    Build dropdown menu and radio buttons to select key signature.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,33 @@ const NavBar = (props) => {
   return (
     <div className="NavBar">
       <button onClick={() => props.createNewSong()}>New Song</button>
-      <button>Choose Key</button>
-      <button>Major Minor</button>
-      <Link to="./poem">Poem</Link>
+      <label for="key-signature">Select a Key</label>
+      <select name="key-signature" id="key-signature">
+        <option value="C">C</option>
+        <option value="C#/Db">C#/Db</option>
+        <option value="D">D</option>
+        <option value="D#/Eb">D#/Eb</option>
+        <option value="E">E</option>
+        <option value="F">F</option>
+        <option value="F#/Gb">F#/Gb</option>
+        <option value="G">G</option>
+        <option value="G#/Ab">G#/Ab</option>
+        <option value="A">A</option>
+        <option value="A#/Bb">A#/Bb</option>
+        <option value="B">B</option>
+      </select>
+      <input 
+        type="radio" 
+        id="major" 
+        name="majmin" 
+        value="major"/>
+      <label for="major">Major</label>
+      <input 
+        type="radio" 
+        id="minor" 
+        name="majmin" 
+        value="minor"/>
+      <label for="minor">Minor</label><br/>
       <Link to="./saved">Saved</Link>
     </div>
   );
