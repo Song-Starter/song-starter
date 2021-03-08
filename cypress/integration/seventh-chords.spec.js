6bee59842de261b24ad732eaea8f1f74c74dc17f
Drew Bradley <williamandrewb@gmail.com>
2021-03-08 09:57:54 -0700

    Test for triads and seventh chords.

---
+++ cypress/integration/seventh-chords.spec.js
@@ -0,0 +1,17 @@
+context('Seventh chords', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  describe('Chords', () => {
+    it('should default to return triads', () => {
+      cy.get('button[class=new-song-button]').click()
+      cy.get('div[class=chord-notes]').find('p').should('have.length', 12)
+    })
+    it('should allow users to select seventh chords', () => {
+      cy.get('div[class=seventh]').find('label').contains('yes').click()
+      cy.get('button[class=new-song-button]').click()
+      cy.get('div[class=chord-notes]').find('p').should('have.length', 16)
+    })
+  })
+})
