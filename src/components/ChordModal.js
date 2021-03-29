0f1961ef0942a8080b37cb9265e0add1cdea7ea6
Drew Bradley <williamandrewb@gmail.com>
2021-03-29 08:45:34 -0600

    Install SVGuitar library.

---
+++ src/components/ChordModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Chord as cord } from 'tonal';
+import './Chord.css';
+
+const Chord = (props) => {
+  const chordNotes = cord.notes(props.name).map(note => <p key={note} className="single-note">{note}</p>)
+  return (
+    <div className="Chord" onClick={() => props.playChord(props.name)}>
+      <p className="chord-name">{props.name}</p>
+      <p className="note-label">notes</p>
+      <div className="chord-notes">{chordNotes}</div>
+      <audio></audio>
+    </div>
+  );
+}
+
+export default Chord;
